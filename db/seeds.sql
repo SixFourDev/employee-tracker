84e1edc4e0e337a8cc9b0ee6797e6e166085945c
Shane <sb_94@yahoo.com>
2023-06-21 12:56:12 -0700

    added seeds for role and employee tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,23 @@ VALUES (001, "Sales"),
        (002, "Engineering"),
        (003, "Finance"),
        (004, "Legal");
+
+INSERT INTO role (id, title, salary)
+VALUES (001, "Sales Lead", 100000),
+       (002, "Salesperson", 80000),
+       (003, "Lead Engineer", 150000),
+       (004, "Software Engineer", 120000),
+       (005, "Account Manager", 160000),
+       (006, "Accountant", 125000),
+       (007, "Legal Team Lead", 250000),
+       (008, "Lawyer", 190000);
+
+INSERT INTO employee (id, first_name, last_name)
+VALUES (001, "John", "Doe"),
+       (002, "Mike", "Chan"),
+       (003, "Ashley", "Rodriguez"),
+       (004, "Kevin", "Tupik"),
+       (005, "Kunal", "Singh"),
+       (006, "Malia", "Brown"),
+       (007, "Sarah", "Lourd"),
+       (008, "Tom", "Allen");
